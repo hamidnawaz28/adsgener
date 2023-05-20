9886584c7f46da41116cb56a16dd769731afac7f
hamidnawaz28 <hamid.nawaz@wearenova.co.uk>
2023-05-20 13:49:17 +0500

    Rfactored

--- src/content-script/components/Input.tsx
+++ src/content-script/components/Input.tsx
@@ -1,5 +1,5 @@
+import { TextField } from '@mui/material'
 import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
 
 export default function Input({ label, value, setValue, variant = 'outlined' }: any) {
   return (
